ee96464ef1cab07230b297cc1ce953ede83ee283
chfe <chris.fey@zuhlke.com>
2016-06-20 10:56:05 +0100

    hello world kotlin

---
+++ app/src/androidTest/java/com/zuhlke/tg_mobile/jira_ar_tgmobile/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.zuhlke.tg_mobile.jira_ar_tgmobile;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
