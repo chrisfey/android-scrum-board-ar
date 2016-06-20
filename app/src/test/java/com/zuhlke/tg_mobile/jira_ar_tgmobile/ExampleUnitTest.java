7cbcbc39ba6c468be1b1b384d995019aa9117e49
chfe <chris.fey@zuhlke.com>
2016-06-20 11:05:26 +0100

    hello world kotlin

---
+++ app/src/test/java/com/zuhlke/tg_mobile/jira_ar_tgmobile/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.zuhlke.tg_mobile.jira_ar_tgmobile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
