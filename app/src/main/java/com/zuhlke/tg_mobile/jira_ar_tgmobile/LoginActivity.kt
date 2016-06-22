8948e96c0ddd3c1a017262b63ab4326eb7f77fa8
Samuel Budd <sabu@samuels-mbp.ads.zuehlke.com>
2016-06-22 13:49:04 +0100

    logo

--- app/src/main/java/com/zuhlke/tg_mobile/jira_ar_tgmobile/LoginActivity.kt
+++ app/src/main/java/com/zuhlke/tg_mobile/jira_ar_tgmobile/LoginActivity.kt
@@ -3,36 +3,26 @@ package com.zuhlke.tg_mobile.jira_ar_tgmobile
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.annotation.TargetApi
-import android.content.pm.PackageManager
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.app.LoaderManager.LoaderCallbacks
-
 import android.content.CursorLoader
+import android.content.Intent
 import android.content.Loader
 import android.database.Cursor
 import android.net.Uri
 import android.os.AsyncTask
-
 import android.os.Build
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
-import android.view.KeyEvent
+import android.util.Log
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.inputmethod.EditorInfo
-import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-
-import java.util.ArrayList
-
-import android.Manifest.permission.READ_CONTACTS
-import android.content.Intent
-import android.util.Log
+import java.util.*
 
 /**
  * A login screen that offers login via email/password.
